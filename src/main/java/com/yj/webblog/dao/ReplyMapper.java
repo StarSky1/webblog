2c0c91c748653c8aaf31e26cebaf32ea55a754db
StarSky <763559353@qq.com>
2018-11-21 23:25:16 +0800

    修改数据库表结构

---
+++ src/main/java/com/yj/webblog/dao/ReplyMapper.java
@@ -0,0 +1,17 @@
+package com.yj.webblog.dao;
+
+import com.yj.webblog.model.domain.Reply;
+
+public interface ReplyMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Reply record);
+
+    int insertSelective(Reply record);
+
+    Reply selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Reply record);
+
+    int updateByPrimaryKey(Reply record);
+}
---
+++ src/main/java/com/yj/webblog/model/domain/Reply.java
@@ -0,0 +1,65 @@
+package com.yj.webblog.model.domain;
+
+import java.util.Date;
+
+public class Reply {
+    private Integer id;
+
+    private Integer parentId;
+
+    private Integer senderId;
+
+    private Integer receiverId;
+
+    private String message;
+
+    private Date sendTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Integer getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Integer senderId) {
+        this.senderId = senderId;
+    }
+
+    public Integer getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Integer receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message == null ? null : message.trim();
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(Date sendTime) {
+        this.sendTime = sendTime;
+    }
+}
