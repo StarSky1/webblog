37168e9fec13f9b9014f69499a590ea1207d9f4f
StarSky <763559353@qq.com>
2018-11-26 22:33:32 +0800

    集成freemarker显示层框架

---
+++ src/test/java/com/yj/webblog/TestFreemarker.java
@@ -0,0 +1,67 @@
+package com.yj.webblog;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author 76355
+ * @Date 2018/11/26 15:42
+ * @Description
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestFreemarker {
+
+    @Test
+    public void test() {
+        File file=new File(".");
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getPath());
+    }
+
+    @Test
+    public void testFreemarker() throws Exception{
+        /* ------------------------------------------------------------------------ */
+        /* You should do this ONLY ONCE in the whole application life-cycle:        */
+
+        /* Create and adjust the configuration singleton */
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
+        cfg.setDirectoryForTemplateLoading(new File(".\\src\\main\\webapp\\WEB-INF\\templates"));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        /* ------------------------------------------------------------------------ */
+        /* You usually do these for MULTIPLE TIMES in the application life-cycle:   */
+
+        /* Create a data-model */
+        Map root = new HashMap();
+        root.put("user", "Big Joe");
+        Map latest = new HashMap();
+        root.put("latestProduct", latest);
+        latest.put("url", "products/greenmouse.html");
+        latest.put("name", "green mouse");
+
+        /* Get the template (uses cache internally) */
+        Template temp = cfg.getTemplate("test.ftl");
+
+        /* Merge data-model with template */
+        FileOutputStream fos=new FileOutputStream(new File("./doc/testFreemarker.html"));
+        Writer out = new OutputStreamWriter(fos);
+        temp.process(root, out);
+        out.close();
+        // Note: Depending on what `out` is, you may need to call `out.close()`.
+        // This is usually the case for file output, but not for servlet output.
+    }
+}
